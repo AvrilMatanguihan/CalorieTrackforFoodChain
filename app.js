a2559360d4d272b7d7577f46205e5eed655a0ed1
Nikki <129667589+Ellyxr@users.noreply.github.com>
2026-02-18 20:42:54 +0800

    added route and connection to db

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+const restaurantRoutes = require('./routes/restaurants');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/restaurants', restaurantRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
@@ -5,6 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
 });
 
 module.exports = pool;
